b2227f5af1a4087d9cad53705496eaa3d9fd7958
Russell Graviet <russgrav@gmail.com>
2022-09-29 13:19:07 +0900

    Create playRound function

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
 function getComputerChoice() {
   randomNumber = Math.random();
+  let computerSelection = "";
   if((randomNumber >= 0) && (randomNumber < 1/3)) {
-    computerChoice = "Rock";
+    computerSelection = "rock";
   } else if ((randomNumber >= 1/3) && (randomNumber < 2/3)) {
-    computerChoice = "Paper";
+    computerSelection = "paper";
   } else {
-    computerChoice = "Scissors";
+    computerSelection = "scissors";
   }
-  return computerChoice;
+  return computerSelection;
 }
 
-getComputerChoice();
-console.log(computerChoice);
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection.toLowerCase()) {
+    case "rock":
+      if (computerSelection === "rock") {
+        result = "tie";
+      } else if (computerSelection === "paper") {
+        result = "loss";
+      } else if (computerSelection === "scissors") {
+        result = "win";
+      }
+      break;
+    case "paper":
+      if (computerSelection === "rock") {
+        result = "win";
+      } else if (computerSelection === "paper") {
+        result = "tie";
+      } else if (computerSelection === "scissors") {
+        result = "loss";
+      }
+      break;
+    case "scissors":
+      if (computerSelection === "rock") {
+        result = "loss";
+      } else if (computerSelection === "paper") {
+        result = "win";
+      } else if (computerSelection === "scissors") {
+        result = "tie";
+      }
+      break;
+  }
+  let playerSelectionFormatted = capitalizeFirstLetter(playerSelection);
+  let computerSelectionFormatted = capitalizeFirstLetter(computerSelection);
+  switch (result) {
+    case "win":
+      return `You win! ${playerSelectionFormatted} beats ${computerSelectionFormatted}.`
+      break;
+    case "loss":
+      return `You lose! ${computerSelectionFormatted} beats ${playerSelectionFormatted}.`
+      break;
+    case "tie":
+      return `It's a tie! ${playerSelectionFormatted} and ${computerSelectionFormatted} are the same.`
+      break;
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  lowerString = string.toLowerCase();
+  return lowerString.charAt(0).toUpperCase() + lowerString.slice(1);
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
